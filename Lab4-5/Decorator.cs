5b9c1da6650b0234cf27eb1de11b597383dea651
Jana <90056190+nesti13@users.noreply.github.com>
2022-06-02 22:50:49 +0300

    Add files via upload

---
+++ Lab4-5/Decorator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4
+{
+    abstract class ComputerDecorator : Computer
+    {
+        public ComputerDecorator(Proc p, Video v, IOZUType o) : base(p, v, o)  //base используется для доступа к членвам базового класса
+        {
+        }
+    }
+    class NewComputer : ComputerDecorator
+    {
+        public NewComputer(Proc p, Video v, IOZUType o) : base(p, v, o)
+        {
+            v.memory.Memory+=2;
+        }
+        public override string ToString()
+        {
+            return "Игровой Компьютер с процессором " + Proc.ToString() + " и видеокартой GEForce: " + Video.ToString() + " и типом оперативной памяти: " + TypeOZU.OZU() + " и подсветкой";
+        }
+    }
+}
---
+++ Lab4-5/Video.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4
+{
+    class VideoMemory
+    {
+        // сколько памяти
+        public int Memory { get; set; }
+    }
+    class SecondCooler
+    { }
+    class DirectX
+    {
+        public string Version { get; set; }
+    }
+
+    class Video
+    {
+        public VideoMemory memory { get; set; }
+        public SecondCooler secondCooler { get; set; }
+        public DirectX directX { get; set; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            if (memory != null)
+                sb.Append("Видеопамять: "+memory.Memory + "\n");
+            if (secondCooler != null)
+                sb.Append(" Два куллера \n");
+            if (directX != null)
+                sb.Append(" Версия DirectX: " + directX.Version + " \n");
+            return sb.ToString();
+        }
+    }
+}
