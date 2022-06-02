12f431c6d2547154b883fe15954f2455b420a149
Jana <90056190+nesti13@users.noreply.github.com>
2022-06-02 22:54:54 +0300

    Add files via upload

---
+++ lab11/Add.xaml.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab11
+{
+    /// <summary>
+    /// Логика взаимодействия для Add.xaml
+    /// </summary>
+    public partial class Add : Window
+    {
+        public byte[] imageBytes;
+        byte a = 1;
+        private master master = new master();
+
+        public Add(master selectmaster)
+        {
+            InitializeComponent();
+            if (selectmaster != null)
+            { master = selectmaster; }
+
+            DataContext = master;
+        }
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            string str = work_expirience.Text;
+            if ((work_expirience.Text.Trim() == "") || (Convert.ToInt32(work_expirience.Text) == 0) || (Convert.ToInt32(work_expirience.Text) < 0))
+            {
+                work_expirience.ToolTip = "Это поле введено не корректно!";
+                work_expirience.BorderBrush = Brushes.Red;
+            }
+            else if (surname.Text == "")
+            {
+                surname.ToolTip = "Это поле введено не корректно!";
+                surname.BorderBrush = Brushes.Red;
+            }
+
+            else if (surname.Text == "")
+            {
+                name.ToolTip = "Это поле введено не корректно!";
+                name.BorderBrush = Brushes.Red;
+            }
+
+            else if (name.Text == "")
+            {
+                education.ToolTip = "Это поле введено не корректно!";
+                education.BorderBrush = Brushes.Red;
+            }
+            else if (education.Text == "")
+            {
+                education.ToolTip = "Это поле введено не корректно!";
+                education.BorderBrush = Brushes.Red;
+            }
+            else if (qualification.Text == "")
+            {
+                qualification.ToolTip = "Это поле введено не корректно!";
+                qualification.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                qualification.ToolTip = "";
+                qualification.BorderBrush = Brushes.Transparent;
+
+
+                try
+                {
+                    lab10Entities.GetContext().masters.Add(master);
+                    lab10Entities.GetContext().SaveChanges();
+                    MessageBox.Show("Информация сохранена!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Hide();
+        }
+
+        private void Photo_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files (*.png;*.jpg)|*.png;*.jpg|All files (*.*)|*.*";
+            if (dialog.ShowDialog() == true)
+            {
+                string path = dialog.FileName;
+                MessageBox.Show(path);
+                byte[] imageBytesBuffer;
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    imageBytesBuffer = new byte[fs.Length];
+                    fs.Read(imageBytesBuffer, 0, imageBytesBuffer.Length);
+                }
+                imageBytes = imageBytesBuffer;
+            }
+        }
+    }
+}
---
+++ lab11/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab11
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string connectionString;
+        SqlDataAdapter adapter;
+        DataTable phonesTable;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Masters.ItemsSource = lab10Entities.GetContext().masters.ToList();
+        }
+
+        private void Masters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void BinEdit_Click(object sender, RoutedEventArgs e)
+        {
+            using (lab10Entities db = new lab10Entities())
+            {
+                db.Database.Log = Console.Write;
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    Add window = new Add((sender as Button).DataContext as master);
+                    var master = Masters.SelectedItems.Cast<master>().ToList();
+                    try
+                    {
+                        lab10Entities.GetContext().masters.RemoveRange(master);
+                        lab10Entities.GetContext().SaveChanges();
+                        Masters.ItemsSource = lab10Entities.GetContext().masters.ToList();
+                        window.Show();
+                        this.Hide();
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                        transaction.Rollback();
+                    }
+                }
+            }
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            using (lab10Entities db = new lab10Entities())
+            {
+                db.Database.Log = Console.Write;
+
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var master = Masters.SelectedItems.Cast<master>().ToList();
+                        if (MessageBox.Show($"Вы точно хотите удалить следующие {master.Count()} элементов?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                        {
+                            try
+                            {
+                                lab10Entities.GetContext().masters.RemoveRange(master);
+                                lab10Entities.GetContext().SaveChanges();
+                                MessageBox.Show("Данные удалены!");
+                                Masters.ItemsSource = lab10Entities.GetContext().masters.ToList();
+                                db.SaveChanges();
+                                transaction.Commit();
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message.ToString());
+                                transaction.Rollback();
+                            }
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                    }
+                }
+            }
+        }
+
+        private void BtnSQL_Click(object sender, RoutedEventArgs e)
+        {
+            using(lab10Entities db = new lab10Entities())
+            {
+                var forDel = db.masters.Where(p => p.name == First.Text);
+                Masters.ItemsSource = forDel.ToList();
+            }
+        }
+
+        private void BtnSQL2_Click(object sender, RoutedEventArgs e)
+        {
+            using (lab10Entities db = new lab10Entities())
+            {
+                var forDel = db.masters.Where(p => p.name == Second.Text);
+                var forDel2 = forDel.Where(p => p.qualification == TRID.Text);
+                Masters.ItemsSource = forDel2.ToList();
+            }
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Add window = new Add(null);
+            window.Show();
+            this.Hide();
+        }
+
+    }
+}
